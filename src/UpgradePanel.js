9d81e4080066950a5390593c0723aea12e566059
Surya Vezhavendan <svezhavendan@gmail.com>
2020-06-24 01:25:42 -0400

    add upgrade panel skeleton/example with 2 tiers of upgrades

--- src/UpgradePanel.js
+++ src/UpgradePanel.js
@@ -2,19 +2,44 @@ import React from 'react';
 import './UpgradePanel.css';
 
 class UpgradePanel extends React.Component {
+
+    state = {tier1num : 10, tier2num : 5}
+
+    addTier1 = () => {
+        this.setState({tier1num : this.state['tier1num'] + 1})
+        console.log(this.state['tier1num'])
+    }
+
+    addTier2 = () => {
+        this.setState({tier2num : this.state['tier2num'] + 1})
+        console.log(this.state['tier2num'])
+    }
+
     render() {
-
-
-
 
         return (
             <div className="UpgradePanel">
-            <p>hi</p>
+
+                <div>  
+                <p>Count: {this.state.tier1num}</p>
+                <button onClick={this.addTier1}> Add a unit! </button>
+                </div>
+
+                <div>  
+                <p>Count: {this.state.tier2num}</p>
+                <button onClick={this.addTier2}> Add a unit! </button>
+                </div>
+
+
+
             </div>
         );
 
 
     };
+
+
+
 }
 
 export default UpgradePanel;
